91c6ab438ff4ef7ca910cc82c33e608ed4c9d2a5
Keichi Takahashi <keichi.t@me.com>
2014-07-06 04:30:55 +0900

    socket.ioを0.9.17にダウングレード

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 server.listen(80);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on("connection", function (socket) {
+io.sockets.on("connection", function (socket) {
 	socket.on("tsukkomi", function (data) {
-		console.log("tsukkomi")
+		console.log("tsukkomi");
 		io.sockets.emit("tsukkomi", {type:"tsukkomi","uid":data.uid});
 	});
 
